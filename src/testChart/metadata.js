cc4320dd038d9f5326de8635ce8e42722fa6ff1a
pauliuszutautas <51474098+pauliuszutautas@users.noreply.github.com>
2024-07-15 15:09:15 +0100

    s

--- src/testChart/metadata.js
+++ src/testChart/metadata.js
@@ -1,6 +1,6 @@
 export const metadata = {
   name: 'Test chart',
-  id: 'rawgraphs.testChart001',
+  id: 'rawgraphs.testchart001',
   category: 'Testing, debugging',
   description: 'a very simple scatterplot',
   code: 'https://observablehq.com/',
